27f5244428336ac8e413fd1d241091fe76d92ae1
Qamar.Hashmi <qamar.hashmi@outlook.com>
2025-03-09 09:47:31 +0530

    java-poc-app

---
+++ src/main/java/com/employee/codebase_poc/CodebasePocApplication.java
@@ -0,0 +1,13 @@
+package com.employee.codebase_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebasePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodebasePocApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/employee/codebase_poc/controller.java
@@ -0,0 +1,13 @@
+package com.employee.codebase_poc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @GetMapping
+    public String message() {
+        return "welcome to code base POC";
+    }
+}
